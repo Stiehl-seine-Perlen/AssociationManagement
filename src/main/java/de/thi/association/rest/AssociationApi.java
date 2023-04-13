26a895676616cbdf95b7044be080f6dfb7cca46d
Maximilian Rublik <maximilian.rublik@hotmail.de>
2023-04-13 09:22:38 +0200

    Enhance Api

--- src/main/java/de/thi/association/rest/AssociationApi.java
+++ src/main/java/de/thi/association/rest/AssociationApi.java
@@ -1,24 +1,32 @@
 package de.thi.association.rest;
 
-import de.thi.association.beans.Association;
-import de.thi.association.entities.AssociationEntry;
+import de.thi.association.entities.Association;
 
 import javax.inject.Inject;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
+import javax.transaction.Transactional;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import java.util.List;
 
 @Path("/association")
 public class AssociationApi {
 
     @Inject
-    Association association;
+    de.thi.association.beans.Association association;
 
     @POST
     @Path("/add")
     @Consumes(MediaType.APPLICATION_JSON)
-    public void addAssociation(AssociationEntry entry) {
+    @Transactional
+    public void addAssociation(Association entry) {
         association.persist(entry);
     }
+
+    @GET
+    @Path("/all")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Association> getAllEntries() {
+        return association.listAll();
+    }
+
 }
