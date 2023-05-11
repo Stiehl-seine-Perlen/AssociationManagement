98e74664217ba49d4791765b95adb471a5a00719
Hubertus Seitz <81434615+Ainges@users.noreply.github.com>
2023-05-11 17:07:34 +0200

    ✨added REST Endpoint to count Memberships of Associations

--- src/main/java/de/thi/association/controller/AssociationResource.java
+++ src/main/java/de/thi/association/controller/AssociationResource.java
@@ -30,4 +30,12 @@ public class AssociationResource {
     public List<Association> getAllEntries() {
         return associationService.getAllAssociations();
     }
+
+    @GET
+    @Path("{associationId}/membercount")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Long getMemberCount(@PathParam("associationId") Long associationid){
+        return  associationService.getMemberCount(associationid);
+    }
 }
--- src/main/java/de/thi/association/repositories/AssociationRepository.java
+++ src/main/java/de/thi/association/repositories/AssociationRepository.java
@@ -7,4 +7,8 @@ import javax.enterprise.context.ApplicationScoped;
 @ApplicationScoped
 public class AssociationRepository implements PanacheRepository<de.benevolo.entities.association.Association> {
     // auto implemented
+
+    public Long getMemberCountOfAssociation(Long associationId){
+         return  find("SELECT m FROM Membership m WHERE m.membershipId = ?1", associationId).count();
+    }
 }
