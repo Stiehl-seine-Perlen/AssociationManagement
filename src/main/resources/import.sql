575ed7004963bd8d0d136657ecb92c52b7028a4c
Matthias Raimann <matthias.raimann82@gmail.com>
2023-05-17 11:45:45 +0200

    Create import.sql

---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO Address VALUES (1, 'Ingolstadt', 'GERMANY', '10', '85049', 'Esplanade');
+
+INSERT INTO Association VALUES (1, 'Neuland e.V.', 'info@neuland.de', 'Verein für Informatikinteressierte', 1);
