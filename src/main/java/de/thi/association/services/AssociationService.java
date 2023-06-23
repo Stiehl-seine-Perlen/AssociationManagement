5df55ec37a167650476991de53736e7af0da8a9e
Philip Lobmeier <72876360+philobmr@users.noreply.github.com>
2023-06-24 00:34:56 +0200

    :recycle: Code Improvement

--- src/main/java/de/thi/association/services/AssociationService.java
+++ src/main/java/de/thi/association/services/AssociationService.java
@@ -18,7 +18,7 @@ import de.thi.association.repositories.MembershipRepository;
 @ApplicationScoped
 public class AssociationService {
 
-    private static final Logger logger = LoggerFactory.getLogger(AssociationService.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(AssociationService.class);
 
     @Inject
     AssociationRepository associationRepository;
@@ -41,7 +41,7 @@ public class AssociationService {
 
             return association;
         } catch (Exception e) {
-            logger.error("Could Not Persist Association", e);
+            LOGGER.error("Could Not Persist Association", e);
             return null;
         }
     }
@@ -62,7 +62,7 @@ public class AssociationService {
             return true;
 
         } catch (Exception e) {
-            logger.error("Update Failed", e);
+            LOGGER.error("Update Failed", e);
             return false;
         }
     }
@@ -73,26 +73,20 @@ public class AssociationService {
             associationRepository.deleteById(id);
             return true;
         } catch (Exception e) {
-            logger.error("Could Not Delete Association With ID: ", id);
+            LOGGER.error("Could Not Delete Association With ID: ", id);
             return false;
         }
     }
 
     @Transactional
-    public Membership persistMembership(Membership membership){
-        //Workaround to addMemberships
-        Membership freshMembership = new Membership();
-        logger.info("Membership: " + membership);
-        membership.setAssociationRole(freshMembership.getAssociationRole());
-        membership.setMembershipId(null);
-        
+    public Membership persistMembership(Membership membership){ 
         try {
             membershipRepository.persist(membership);
-            logger.info("Membership persisted.");
-           return membership;
-        } catch (Exception e) {
-           logger.error("Could Not Persist Membership", e);
+            LOGGER.info("Membership persisted.");
             return membership;
+        } catch (Exception e) {
+            LOGGER.error("Could Not Persist Membership", e);
+            return null;
         }
     }
 }
