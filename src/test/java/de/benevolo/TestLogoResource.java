70fac3c22c06478632ec4363f885b3c5d5af2fae
Maximilian Rublik <maximilian.rublik@hotmail.de>
2023-07-03 01:57:26 +0200

    Add logo setup

--- src/test/java/de/benevolo/TestLogoResource.java
+++ src/test/java/de/benevolo/TestLogoResource.java
@@ -2,14 +2,20 @@ package de.benevolo;
 
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.RestAssured;
+import org.eclipse.microprofile.config.ConfigProvider;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.Assert;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.wait.strategy.HttpWaitStrategy;
 
 import javax.ws.rs.BadRequestException;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
+import java.time.Duration;
+import java.util.Arrays;
 
 import static io.restassured.RestAssured.given;
 
@@ -17,6 +23,29 @@ import static io.restassured.RestAssured.given;
 @QuarkusTest
 public class TestLogoResource {
 
+    @BeforeAll
+    public static void setup() throws Exception {
+
+        // Arrange
+        String accessKey = ConfigProvider.getConfig().getValue("minio.accessKey",
+                String.class);
+        String secretKey = ConfigProvider.getConfig().getValue("minio.secretKey",
+                String.class);
+
+        GenericContainer minioServer = new GenericContainer("minio/minio")
+                .withEnv("MINIO_ACCESS_KEY", accessKey)
+                .withEnv("MINIO_SECRET_KEY", secretKey)
+                .withCommand("server /data")
+                .withExposedPorts(9000, 9001)
+                .waitingFor(new HttpWaitStrategy()
+                        .forPath("/minio/health/ready")
+                        .forPort(9000)
+                        .withStartupTimeout(Duration.ofSeconds(10)));
+
+        minioServer.setPortBindings(Arrays.asList("9000:9000", "9001:9001"));
+        minioServer.start();
+
+    }
     @Test
     public void testAddAssociationLogo() {
         String filePath = String.format("%s%s", System.getProperty("user.dir"),
